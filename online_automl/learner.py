cf5f924bce45a0bfe9dd6dd93cda46a117b93221
qingyun-wu <qw2ky@virginia.edu>
2020-12-17 17:22:49 -0800

    refactor learner

--- online_automl/learner.py
+++ online_automl/learner.py
@@ -13,7 +13,8 @@ from functools import partial, partialmethod
 from typing import Dict, Optional, List, Tuple
 from util import strip_noninteractive_feature
 from sklearn.metrics import mean_squared_error
-
+import logging
+logger = logging.getLogger(__name__)
 
 class TrainableVWTrial:
 
@@ -43,7 +44,7 @@ class TrainableVWTrial:
     """
     model_class = pyvw.vw
     cost_unit = 1.0
-    const = 1.0
+    const = 0.5
     # quartic_config_key = 'q'
     # cubic_config_key = 'cubic'
     interactions_config_key = 'interactions'
@@ -51,7 +52,9 @@ class TrainableVWTrial:
     ignore_config_key = 'ignore'
     keep_config_key = 'keep'
     redefine_config_key = 'redefine'
-    def __init__(self, trial_id, feature_set, fixed_config, init_result): 
+    LOSS_MIN = 0
+    LOSS_MAX = np.inf
+    def __init__(self, trial_id, feature_set, namespace_feature_dim, fixed_config, init_result): 
         """ need to provide init_result
             #TODO: the config should be a set instead of a list
         """
@@ -62,7 +65,7 @@ class TrainableVWTrial:
         self.trained_model = TrainableVWTrial.model_class(
             **self._vw_fs_config, **self._fixed_config )
         # TODO: how to get the dim of parameters
-        self._dim = len(self._feature_set)*3
+        self._dim = self.get_dim(namespace_feature_dim, feature_set)
         self._resouce_used_attr = 'resource_used'
         self._resource_budget_attr = 'resource_budget'
         self._result = {'config': self._feature_set,}
@@ -71,6 +74,16 @@ class TrainableVWTrial:
             if k != 'config': self._result[k] = v
         #TODO: why I can not do this? 
         # self._result['config']  = copy.deepcopy(self._feature_set) 
+
+    @staticmethod
+    def get_dim(namespace_feature_dim, namespace_set):
+        #TODO: how to decide the dimensionality of the interaction features?
+        dim = 0
+        for f in namespace_set:
+            for c in f:
+                dim += namespace_feature_dim[c]
+        logger.debug('dim %s %s', dim, namespace_set)
+        return dim
 
     def train_vw(self, data, y, old_result):
         """ train vw model
@@ -93,9 +106,11 @@ class TrainableVWTrial:
         # print('loss sum', loss_sum)
         self._result['loss_avg'] =  self._result['loss_sum']/self._result['data_sample_count']
         self._result['cb'] =  TrainableVWTrial.const*math.sqrt(self._dim/self._result['data_sample_count']) 
-        self._result['loss_ucb'] = self._result['loss_avg'] + self._result['cb']
-        self._result['loss_lcb'] = self._result['loss_avg'] - self._result['cb']
-        
+        self._result['loss_ucb'] = min(self._result['loss_avg'] + self._result['cb'],
+            TrainableVWTrial.LOSS_MAX)
+        self._result['loss_lcb'] = max(self._result['loss_avg'] - self._result['cb'], 
+            TrainableVWTrial.LOSS_MIN)
+        logger.debug('result %s', self._result)
     def update_res_budget_in_result(self, result):
         """ the resource update usually needs to be updated externally 
         by some algorithm or scheduler
@@ -133,15 +148,16 @@ class AutoVW:
     learner_class = pyvw.vw
     MAX_NUM = 2**16
     def __init__(self, min_resource_budget: int, policy_budget: int, 
-        init_feature_set: set, fixed_hp_config: dict, scheduler_name: str=None, **kwargs):
-        from AML.blendsearch.trial_runner import OnlineTrialRunner
+        namespace_feature_dim: dict, inter_order: int, fixed_hp_config: dict, scheduler_name: str=None, **kwargs):
+        from AML.blendsearch.trial_runner import OnlineTrialRunnerWithChampion
         from ray.tune.schedulers import FIFOScheduler, ASHAScheduler, HyperBandScheduler
         from AML.blendsearch.scheduler.online_scheduler import OnlineDoublingScheduler 
         from AML.blendsearch.searcher.online_searcher import BaseFeatureSearcher, FeatureInteractionSearcher 
         self._min_resource_budget = min_resource_budget
         self._policy_budget = policy_budget
         self._fixed_hp_config = fixed_hp_config
-        self._init_feature_set = init_feature_set
+        self._init_feature_set = set(namespace_feature_dim.keys())
+        self._namespace_feature_dim = namespace_feature_dim
         self._learner_class = AutoVW.learner_class
         self._best_trial = None 
         self._all_trials = {} # trial_id -> trial
@@ -156,6 +172,9 @@ class AutoVW:
         'cb': 1.0,
         'loss_ucb': np.inf, 
         'loss_lcb': np.inf,
+        'is_newly_promoted_champion': False,
+        'is_champion': False,
+        'priority_factor': 1.0,
         }
         self._loss_func = self._get_loss_func_from_config(self._fixed_hp_config)
         # the incumbent_learner is the incumbent vw when the AutoVW is called
@@ -171,12 +190,14 @@ class AutoVW:
         #     init_feature_set = init_feature_set,
         #     init_result = self._init_result,
         #     )
+        self._iter = 0
 
         self._searcher = FeatureInteractionSearcher(
             metric='loss_ucb',
             mode='min',
-            init_feature_set = init_feature_set,
+            init_feature_set = self._init_feature_set,
             init_result = self._init_result,
+            inter_order = inter_order,
             )    
     
         online_scheduler = OnlineDoublingScheduler(
@@ -206,7 +227,7 @@ class AutoVW:
         else:
             self._scheduler = online_scheduler
         
-        self._trial_runner = OnlineTrialRunner(
+        self._trial_runner = OnlineTrialRunnerWithChampion(
             search_alg=self._searcher,
             scheduler= self._scheduler,
             running_budget=self._policy_budget,
@@ -240,14 +261,16 @@ class AutoVW:
         # if trial does not exist, create a new model
         if trial.trial_id not in self._learner_dic:
             self._learner_dic[trial.trial_id] = TrainableVWTrial(trial.trial_id, 
-                trial.config, self._fixed_hp_config, self._init_result)
+                trial.config, self._namespace_feature_dim, self._fixed_hp_config, self._init_result)
         # get trial result
         self._learner_dic[trial.trial_id].update_res_budget_in_result(trial.result)
         # train the model for one step
         self._learner_dic[trial.trial_id].train_vw(data, y, trial.result)
         # update trial result
+        #TODO: should not change the result of a trial out side the trial runner
         trial.result = self._learner_dic[trial.trial_id].get_result() 
-           
+        return self._learner_dic[trial.trial_id].get_result() 
+
     def predict(self, x):
         """ Predict on the input example
         """
@@ -266,24 +289,28 @@ class AutoVW:
         # clean up old models from self._learner_dics
         self._clean_up_old_models(scheduled_trials) 
         for trial in scheduled_trials:
-            self._train_vw_trial(trial, x, y)
-        self._trial_runner.process_trials_result(scheduled_trials)
-        self._best_trial = self._trial_runner.get_best_running_trial()
+            result = self._train_vw_trial(trial, x, y)
+            self._trial_runner.process_trial_result(trial, result)
+        self._trial_runner.process_running_trials(scheduled_trials)
+        self._best_trial = self._select_trial_for_prediction(scheduled_trials, self._metric, self._mode)
         self._incumbent_learner = self._learner_dic[self._best_trial.trial_id].trained_model
+        self._iter +=1
 
     def _clean_up_old_models(self, scheduled_trials):
 
-        last_running_trials = list(self._learner_dic.keys())
-        new_trials = [trial.trial_id for trial in scheduled_trials]
-        removed_ones = [x for x in last_running_trials if x not in new_trials]
-        for trail_id in removed_ones:
-            print( 'clean up', trail_id, self._learner_dic[trail_id]._feature_set)
-            # raise ValueError
-            del self._learner_dic[trail_id]
-        # print('clean up old',)
+        last_running_trial_ids = list(self._learner_dic.keys())
+        scheduled_trial_ids = [trial.trial_id for trial in scheduled_trials]
+        removed_ones = [x for x in last_running_trial_ids if x not in scheduled_trial_ids]
+        newly_added_ones = [x for x in scheduled_trial_ids if x not in last_running_trial_ids]
+        if removed_ones:
+            logger.debug('====clean up at iteration %s====', self._iter)
+            logger.debug('    clean up %s models, including %s ', len(removed_ones), removed_ones)
+            logger.debug('    add      %s models, including %s', len(newly_added_ones), newly_added_ones)
+            logger.debug('now running  %s models, including %s', len(scheduled_trial_ids), scheduled_trial_ids)
+            for trail_id in removed_ones:
+                del self._learner_dic[trail_id]
         
     def _update_sum_loss(self, y):
-
         loss = self._loss_func([self._y_pred], y)
         # destory self._y_predict after calculating the loss
         self._y_pred = None
@@ -295,6 +322,20 @@ class AutoVW:
 
     def finished(self):
         return self._incumbent_learner.finished()
+
+    @staticmethod
+    def _select_trial_for_prediction(running_trials, metric, mode):
+        best_score = float('+inf') if mode=='min' else float('-inf')
+        best_trial = None
+        if len(running_trials)!=0:
+            for trial in running_trials:
+                score = trial.result[metric]
+                if 'min' == mode and  score < best_score or \
+                    'max' == mode and score > best_score:
+                    best_score = score
+                    best_trial = trial
+        logger.info('trial for prediction %s', best_trial.config)
+        return best_trial
     
     
 
